34bdd3ba6c65699a0832a3184171d75d7601713e
Harald Wilbertz <harald.wilbertz@gmx.de>
2021-04-04 15:03:16 +0200

    Typo in filename corrected.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.components'
+import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 
