4038410ac24c1a9a1af673a8359a48490bdf5db0
Harald Wilbertz <harald.wilbertz@gmx.de>
2021-04-08 20:06:57 +0200

    Reactive form added.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router'
+import { AuthService } from './auth.service'
 
 @Component({
     templateUrl: './profile.component.html'
@@ -8,13 +10,25 @@ import { FormControl, FormGroup } from '@angular/forms'
     
     profileForm!: FormGroup;
 
+    constructor(private authService: AuthService, private router: Router) {    
+    }
+
     ngOnInit() {
-      let firstName = new FormControl();
-      let lastName = new FormControl();
+      let firstName = new FormControl(this.authService.currentUser.firstName);
+      let lastName = new FormControl(this.authService.currentUser.lastName);
 
       this.profileForm = new FormGroup({
         firstName: firstName,
         lastName: lastName
       })
+    }
+
+    save(formValues: any) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['events']);
+    }
+
+    cancel() {
+      this.router.navigate(['events']);
     }
   }
