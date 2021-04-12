50022eef710111cf14bbc5525a01e3691b359ff9
Harald Wilbertz <harald.wilbertz@gmx.de>
2021-04-12 21:36:27 +0200

    Work in progress.

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
    templateUrl: './create-session.component.html' 
 })
-export class CreateSessionComponent {
+export class CreateSessionComponent implements OnInit{
 
+    name!: FormControl;
+    presenter!: FormControl;
+    duration!: FormControl;
+    level!: FormControl;
+    abstract!: FormControl;
+    newSessionForm!: FormGroup;
+
+    ngOnInit() {
+        this.name = new FormControl('', Validators.required);
+        this.presenter = new FormControl('', Validators.required);
+        this.duration = new FormControl('', Validators.required);
+        this.level = new FormControl('', Validators.required);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+
+        this.newSessionForm = new FormGroup({
+            name: this.name,
+            presenter: this.presenter,
+            duration: this.duration,
+            level: this.level,
+            abstract: this.abstract
+        })
+    }
 }
