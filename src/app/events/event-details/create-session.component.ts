43d698f55be7eacc4406e1d99ab6fb5bf0c010ef
Harald Wilbertz <harald.wilbertz@gmx.de>
2021-04-12 23:00:46 +0200

    custom validator added.

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators} from '@angular/forms'
-import { ISession } from '../shared/index'
+import { ISession, restrictedWords } from '../shared/index'
 
 @Component({
    templateUrl: './create-session.component.html', 
    styles: [`
    em { float: right; color: #E05C65; padding-left: 10px; }
-   .error input, .error select, error textarea { background-color: #E3C3C5; }
+   .error input, .error select, .error textarea { background-color: #E3C3C5; }
    .error ::-webkit-input-placeholder { color: #999; } 
    .error :-moz-placeholder { color: #999; }
    .error :ms-input-placeholder { color: #999; }
@@ -26,7 +26,7 @@ export class CreateSessionComponent implements OnInit{
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
