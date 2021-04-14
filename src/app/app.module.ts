60ef441758eef04e3e4a557a82aa8ad3400b5fb1
Harald Wilbertz <harald.wilbertz@gmx.de>
2021-04-14 12:10:37 +0200

    Collapsible well component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ import {
 
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component'
-import { ToastrService} from './common/toastr.service'
+import { ToastrService } from './common/toastr.service'
+import { CollapsibleWellComponent } from './common/collapsible-well.component'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
@@ -38,7 +39,8 @@ import { AuthService } from './user/auth.service'
     NavBarComponent,
     Error404Component,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    CollapsibleWellComponent
   ],
   providers: [
     EventService, 
